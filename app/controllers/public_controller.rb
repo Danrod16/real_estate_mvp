d79cbaecab0b12d227eeaf5a13167221773f8523
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-01-28 14:54:50 +0100

    feat: Auth forms created

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,5 @@
 class PublicController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:main]
+  # skip_before_action :authenticate_user!, only: [:main]
   def main
   end
 end
