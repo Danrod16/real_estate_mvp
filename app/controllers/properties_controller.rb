a90dd671c176e93d55b9ef25da4b2a127b473d69
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-01-29 12:08:57 +0100

    FEAT: photo Uploader

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -69,6 +69,6 @@ class PropertiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def property_params
-      params.require(:property).permit(:name, :address, :price, :rooms, :bathrooms)
+      params.require(:property).permit(:name, :address, :price, :rooms, :bathrooms, :photo)
     end
 end
